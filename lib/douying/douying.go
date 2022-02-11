2b00a7639332aa129de66a181570f0aafe6e630e
kangjr <1569482471@qq.com>
2022-02-11 16:26:52 +0800

    -抖音小程序  公共库

--- lib/douying/douying.go
+++ lib/douying/douying.go
@@ -4,6 +4,25 @@ import (
 	"github.com/fastwego/microapp"
 )
 
+type CommonError struct {
+	Errcode int    `json:"errcode"`
+	Errmsg  string `json:"errmsg"`
+	Error   int    `json:"error"`
+	Message string `json:"message"`
+}
+
+type CodeSession struct {
+	SessionKey      string `json:"session_key"`
+	OpenID          string `json:"openid"`
+	AnonymousOpenid string `json:"anonymous_openid"`
+	Unionid         string `json:"unionid"`
+}
+
+type ResCodeSession struct {
+	CommonError
+	CodeSession
+}
+
 var MicroappApp *microapp.MicroApp
 
 func InitMicroapp(conf microapp.Config) {
--- tests/lib/douyin/douyin_test.go
+++ tests/lib/douyin/douyin_test.go
@@ -1,16 +1,33 @@
 package douyin
 
 import (
+	"encoding/json"
 	"github.com/fastwego/microapp"
+	"github.com/fastwego/microapp/apis/auth"
 	"github.com/gogf/gf/frame/g"
 	"github.com/kdcer/go-lib/lib/douying"
+	"net/url"
 	"testing"
 )
 
 //初始化 抖音小程序配置
 func Test_douyin(t *testing.T) {
 	douying.InitMicroapp(microapp.Config{
-		AppId:     g.Config().GetString("douyin.AppId"),
-		AppSecret: g.Config().GetString("douyin.AppSecret"),
+		AppId:     g.Config().GetString("douyin.appId"),
+		AppSecret: g.Config().GetString("douyin.appSecret"),
 	})
+}
+
+//exp 抖音code登陆 演示
+func Test_douyin_login(t *testing.T) {
+	douying.InitMicroapp(microapp.Config{
+		AppId:     g.Config().GetString("douyin.appId"),
+		AppSecret: g.Config().GetString("douyin.appSecret"),
+	})
+	var tk douying.ResCodeSession
+
+	params := url.Values{}
+	params.Add("code", "code")
+	resp, _ := auth.Code2Session(douying.MicroappApp, params)
+	json.Unmarshal(resp, &tk)
 }
